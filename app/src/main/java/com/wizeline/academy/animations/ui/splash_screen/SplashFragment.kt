ef39fa50b6f53a19ca20687df0631d5e50a87e93
EDUARDO CHAVEZ <41527710+RainbowChvz@users.noreply.github.com>
2022-05-23 17:59:41 -0700

    Animate the logo with a ViewPropertyAnimator

--- app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
@@ -1,14 +1,17 @@
 package com.wizeline.academy.animations.ui.splash_screen
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.BounceInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.wizeline.academy.animations.databinding.SplashFragmentBinding
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -28,9 +31,37 @@ class SplashFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+
+        animateLogo()
+
         lifecycleScope.launch {
             delay(2000)
             goToHomeScreen()
+        }
+    }
+
+    /**
+    Animate the logo using an ObjectAnimator or ViewPropertyAnimator with at least 2 properties
+    animated. Once the logo animation ends go to next fragment using a View.animation xml (set
+    on the navigation graph)
+     */
+    private fun animateLogo() {
+
+        val alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 0F, 1F, 0.1F)
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 1F, 0.5F, 1.5F, 1F)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 1F, 0.5F, 1.5F, 1F)
+        val rotate = PropertyValuesHolder.ofFloat(View.ROTATION, 0F, 360F)
+
+        ObjectAnimator.ofPropertyValuesHolder(
+            binding.ivWizelineLogo,
+            alpha,
+            scaleX,
+            scaleY,
+            rotate
+        ).apply {
+            duration = 2000
+            interpolator = BounceInterpolator()
+            start()
         }
     }
 
